952b7dc2fbc3bf3652b2857079f5a8f3ae1d36c8
Md. Rakibul Islam <mri.bscse@gmail.com>
2025-05-01 00:45:49 +0600

    Add files via upload

---
+++ 1 to n sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{int i=1,n,sum=0;
+    printf(" Enter any number:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++ )
+    {
+        sum=sum+i;
+    }
+        printf("%d",sum);
+
+
+
+    return 0;
+}
---
+++ store input and printing output.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+int i,values[5];
+printf("Enter 5 integers: ");
+// taking input and storing it in an array
+//for(int i = 0; i < 5; ++i) {
+scanf("%d", &values[i]);
+//}
+printf("Displaying integers: \n");
+// printing elements of an array
+for(int i = 0; i < 5; ++i) {
+printf("%d\n", values[i]);
+}
+return 0;
+}
